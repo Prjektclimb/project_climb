31aa04e98c7404d10fd085aba211803207c65fb2
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-11-12 20:26:42 -0500

    updated settings page

---
+++ src/components/client/ProfilePreferences.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import React, { useState, ChangeEvent, FormEvent } from "react";
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export function ProfilePreferences() {
+  const [formData, setFormData] = useState<FormValues>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+    // You can handle form submission logic here, e.g., sending data to a server
+    console.log("Form submitted:", formData);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='self-auto'
+    >
+      <div className="mb-4">
+        <label htmlFor="firstName" className="block text-sm font-medium text-gray-600">
+          First Name:
+        </label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          className="mt-1 p-2 w-full border rounded-md"
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="lastName" className="block text-sm font-medium text-gray-600">
+          Last Name:
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          className="mt-1 p-2 w-full border rounded-md"
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className="mt-1 p-2 w-full border rounded-md"
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-600">
+          Phone Number:
+        </label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          className="mt-1 p-2 w-full border rounded-md"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+
