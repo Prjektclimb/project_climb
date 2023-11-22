0df8a905d54b584b69d51fecf812f24516b2b313
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-11-21 21:38:14 -0500

    updated stateList component to advoid error

--- src/app/gyms/page.tsx
+++ src/app/gyms/page.tsx
@@ -4,7 +4,6 @@ export default function gymPage() {
 	return ( 
 		<>
 		<h1>Hello State List</h1>
-		
 		</>
 	)
 }
--- src/components/client/stateList.tsx
+++ src/components/client/stateList.tsx
@@ -5,14 +5,11 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 import Link from "next/link"
 
 export default function StateList() {
-	
-	const url = window.location.origin
-	
-	
+		
   return (
 	<div>
 		<ScrollArea className="h-100% w-[350px] rounded-md border p-4 pb-24" >
-			{states.map((state) => <li><Link href={`${url}/gyms/${state}`} key={state}>{state}</Link>
+			{states.map((state) => <li key={state}><Link href={`/gyms/${state}`}>{state}</Link>
 			</li>)}
 		</ScrollArea> 
 	</div>
