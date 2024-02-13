a238596a03ce0006f66ae4e7255f75efdbe80b86
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-02-13 16:06:37 -0500

    created a function to link to state type

--- src/components/client/GymComponents/GymsCompleteList.tsx
+++ src/components/client/GymComponents/GymsCompleteList.tsx
@@ -8,9 +8,12 @@ import {
   AllGymInfoFetchOrUndefine,
   GymInfoOrUndefined,
 } from "~/types/supabasetypes";
+import Link from "next/link";
+import { getStateFullName } from "~/functions&hooks/general_functions";
 
 export default async function GymsCompleteList() {
   const [gyms, setGyms] = useState<AllGymInfoFetchOrUndefine[]>([]);
+  
 
   useEffect(() => {
     const fetchGyms = async () => {
@@ -26,22 +29,21 @@ export default async function GymsCompleteList() {
   return (
     <>
       <ScrollArea className="mb-8 space-x-4 rounded-md  border bg-sky-50 p-4  shadow-md">
-        <div className="p-4 lg:h-48 md:full overflow-scroll">
-
-        {gyms ? (
-          <>
-          <ul>
-            {gyms.map((gym, index) => (
-              <li key={index} className="flex flex-row justify-between">
-                <p>{gym?.gym}</p>
-                <p className="font-extralight">...{gym?.state}</p>
-              </li>
-            ))}
-          </ul>
-          </>
-        ) : (
-          <p>No Gym info</p>
-        )}
+        <div className="md:full overflow-scroll p-4 lg:h-48">
+          {gyms ? (
+            <>
+              <ul>
+                {gyms.map((gym, index) => (
+                  <li key={index} className="flex flex-row justify-between">
+                    <Link href={`/gyms/${getStateFullName(gym?.state)}`}>{gym?.gym}</Link>
+                    <p className="font-extralight">...{gym?.state}</p>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <p>No Gym info</p>
+          )}
         </div>
       </ScrollArea>
     </>
--- src/functions&hooks/general_functions.ts
+++ src/functions&hooks/general_functions.ts
@@ -8,3 +8,91 @@ export function formatState(state: string) {
 
   return formattedState;
 }
+
+
+
+export function getStateFullName(state: string | undefined): string | null | undefined{
+  const states: { [key: string]: string } = {
+      'al': 'Alabama',
+      'ak': 'Alaska',
+      'az': 'Arizona',
+      'ar': 'Arkansas',
+      'ca': 'California',
+      'co': 'Colorado',
+      'ct': 'Connecticut',
+      'de': 'Delaware',
+      'fl': 'Florida',
+      'ga': 'Georgia',
+      'hi': 'Hawaii',
+      'id': 'Idaho',
+      'il': 'Illinois',
+      'in': 'Indiana',
+      'ia': 'Iowa',
+      'ks': 'Kansas',
+      'ky': 'Kentucky',
+      'la': 'Louisiana',
+      'me': 'Maine',
+      'md': 'Maryland',
+      'ma': 'Massachusetts',
+      'mi': 'Michigan',
+      'mn': 'Minnesota',
+      'ms': 'Mississippi',
+      'mo': 'Missouri',
+      'mt': 'Montana',
+      'ne': 'Nebraska',
+      'nv': 'Nevada',
+      'nh': 'New Hampshire',
+      'nj': 'New Jersey',
+      'nm': 'New Mexico',
+      'ny': 'New York',
+      'nc': 'North Carolina',
+      'nd': 'North Dakota',
+      'oh': 'Ohio',
+      'ok': 'Oklahoma',
+      'or': 'Oregon',
+      'pa': 'Pennsylvania',
+      'ri': 'Rhode Island',
+      'sc': 'South Carolina',
+      'sd': 'South Dakota',
+      'tn': 'Tennessee',
+      'tx': 'Texas',
+      'ut': 'Utah',
+      'vt': 'Vermont',
+      'va': 'Virginia',
+      'wa': 'Washington',
+      'wv': 'West Virginia',
+      'wi': 'Wisconsin',
+      'wy': 'Wyoming'
+  };
+
+  // Convert state abbreviation to lowercase
+  state = state?.toLowerCase();
+
+  // Check if the state abbreviation exists in the states object
+  if(state !== undefined && states[state] !== undefined) {
+  if (states[state]) {
+    return states[state]?.toLowerCase();
+}
+  } 
+
+  // Check if the full name of the state exists in the states object
+  if (states !== undefined) {
+  for (const abbr in states) {
+      if (states.hasOwnProperty(abbr)) {
+          if ( states[abbr]?.toLowerCase() === state) {
+              return states[abbr]?.toLowerCase();
+          }
+      }
+  }
+} 
+
+  return null; 
+
+}
+
+
+
+
+
+
+
